f0503a86e95039e208f43eff6479e39a149fbaba
Kevin Sena <senakevin50@gmail.com>
2022-06-01 18:05:25 -0300

    feat: setup database

---
+++ app/backend/src/database/models/MatchModel.ts
@@ -0,0 +1,55 @@
+import { Model, INTEGER, BOOLEAN } from 'sequelize';
+import db from '.';
+import Teams from './TeamModel';
+
+class Matches extends Model {
+  id!: number;
+  homeTeam!: number;
+  homeTeamGoals!: number;
+  awayTeam!: number;
+  awayTeamGoals!: number;
+  inProgress!: boolean;
+}
+
+Matches.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: INTEGER,
+  },
+  homeTeam: {
+    allowNull: false,
+    type: INTEGER,
+    field: 'home_team'
+  },
+  homeTeamGoals: {
+    allowNull: false,
+    type: INTEGER,
+    field: 'home_team_goals'
+  },
+  awayTeam: {
+    allowNull: false,
+    type: INTEGER,
+    field: 'away_team'
+  },
+  awayTeamGoals: {
+    allowNull: false,
+    type: INTEGER,
+    field: 'away_team_goals'
+  },
+  inProgress: {
+    allowNull: false,
+    type: BOOLEAN,
+    field: 'in_progress',
+  }
+}, {
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+});
+
+Matches.belongsTo(Teams, { foreignKey: 'homeTeam' });
+Matches.belongsTo(Teams, { foreignKey: 'awayTeam' })
+
+export default Matches;
---
+++ app/backend/src/database/models/TeamModel.ts
@@ -0,0 +1,27 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class Teams extends Model {
+  id!: number;
+  teamName!: string;
+}
+
+Teams.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: INTEGER,
+  },
+  teamName: {
+    allowNull: false,
+    type: STRING(50),
+    field: 'team_name'
+  }
+}, {
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+});
+
+export default Teams;
