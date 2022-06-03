c09e866234afe13aa0638e30854ca1298887ea50
Kevin Sena <senakevin50@gmail.com>
2022-06-03 00:20:57 -0300

    fix: change return for bad requests

--- app/backend/src/services/LoginService.ts
+++ app/backend/src/services/LoginService.ts
@@ -8,19 +8,25 @@ import { ILoginService } from './ServiceInterfaces';
 
 export default class LoginService implements ILoginService {
   private unauthorizedMessage: string;
+  private blankSpaceMessage: string;
   constructor(private model = Users) {
     this.unauthorizedMessage = 'Incorrect email or password';
+    this.blankSpaceMessage = 'All fields must be filled';
+  }
+
+  async validatePassword(comingPassword: string, password: string): Promise<void> {
+    const isValid = await compare(comingPassword, password);
+    if (!isValid) throw new ResError(this.unauthorizedMessage, StatusCodes.UNAUTHORIZED);
   }
 
   async getToken(email: string, comingPassword: string): Promise<object> {
     if (!email || !comingPassword) {
-      throw new ResError(this.unauthorizedMessage, StatusCodes.UNAUTHORIZED);
+      throw new ResError(this.blankSpaceMessage, StatusCodes.BAD_REQUEST);
     }
     const user = await this.model.findOne({ where: { email } });
     if (!user) throw new ResError(this.unauthorizedMessage, StatusCodes.UNAUTHORIZED);
     const { id, username, role, password } = user;
-    const isValid = await compare(comingPassword, user.password);
-    if (!isValid) throw new ResError(this.unauthorizedMessage, StatusCodes.UNAUTHORIZED);
+    this.validatePassword(comingPassword, password);
     const token = sign({ id, email, role, password }, config.secret, config.configs);
     return {
       user: { id, username, role, email },
